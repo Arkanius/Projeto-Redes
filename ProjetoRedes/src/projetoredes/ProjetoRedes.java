3055fe6601cfedf46d46ab88a5a1f8759c7a0d90
Osni <osni.coj@hotmail.com>
2015-07-19 18:55:01 -0300

    Alteração no tema

--- ProjetoRedes/src/projetoredes/ProjetoRedes.java
+++ ProjetoRedes/src/projetoredes/ProjetoRedes.java
@@ -5,6 +5,11 @@
  */
 package projetoredes;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Victor
@@ -15,6 +20,18 @@ public class ProjetoRedes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        try {
+            UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProjetoRedes.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ProjetoRedes.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ProjetoRedes.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(ProjetoRedes.class.getName()).log(Level.SEVERE, null, ex);
+        }
         telaPrincipal tp = new telaPrincipal();
         tp.setVisible(true);
     }
